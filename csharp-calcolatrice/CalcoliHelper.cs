bc821e36b87163ce393f1b8e9ed785523c5202e3
Andrea Bonaventura <andreabonaventura802@gmail.com>
2023-04-12 14:59:22 +0200

    Aggiungere i file di progetto.

---
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_calcolatrice
+{
+    internal class CalcoliHelper
+    {
+        private CalcoliHelper()
+        {
+            // Il costruttore privato per evitare la creazione dell'istanza
+        }
+
+        public static int SommaNum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static double SommaNum(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int DifferenzaNum(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public static double DifferenzaNum(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public static int ProdottoNum(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static double ProdottoNum(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public static int ValAbsNum(int num)
+        {
+            return Math.Abs(num);
+        }
+
+        public static double ValAbsNum(double num)
+        {
+            return Math.Abs(num);
+        }
+
+        public static int MinNum(int num1, int num2)
+        {
+            return Math.Min(num1, num2);
+        }
+
+        public static double MinNum(double num1, double num2)
+        {
+            return Math.Min(num1, num2);
+        }
+
+        public static int MaxNum(int num1, int num2)
+        {
+            return Math.Max(num1, num2);
+        }
+
+        public static double MaxNum(double num1, double num2)
+        {
+            return Math.Max(num1, num2);
+        }
+    }
+}
---
+++ csharp-calcolatrice/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace csharp_calcolatrice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("1)" + CalcoliHelper.SommaNum(2, 2));
+            Console.WriteLine("2)" + CalcoliHelper.SommaNum(2.5, 3.7));
+
+            Console.WriteLine("3)" + CalcoliHelper.DifferenzaNum(2, 2));
+            Console.WriteLine("4)" + CalcoliHelper.DifferenzaNum(2.5, 3.7));
+
+            Console.WriteLine("5)" + CalcoliHelper.ProdottoNum(2, 2));
+            Console.WriteLine("6)" + CalcoliHelper.ProdottoNum(2.5, 3.7));
+
+            Console.WriteLine("7)" + CalcoliHelper.ValAbsNum(-2));
+            Console.WriteLine("8)" + CalcoliHelper.ValAbsNum(-2.5));
+
+            Console.WriteLine("9)" + CalcoliHelper.MinNum(2,4));
+            Console.WriteLine("10)" + CalcoliHelper.MinNum(2.5, 3.7));
+
+            Console.WriteLine("11)" + CalcoliHelper.MaxNum(2, 4));
+            Console.WriteLine("12)" + CalcoliHelper.MaxNum(2.5, 3.7));
+
+
+        }
+    }
+}
